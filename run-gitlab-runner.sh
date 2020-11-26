8d124983df7c9ee70d1bf8a86fa43a719568d4e8
nikaorl <nika.orlova.1997@yandex.ru>
2020-11-26 14:40:29 +0300

    add scripts

---
+++ run-gitlab-runner.sh
@@ -0,0 +1,6 @@
+sudo docker run -d --network myNetwork \
+     --name gitlab-runner \
+     --restart always \
+     -v /srv/gitlab-runner/config:/etc/gitlab-runner \
+     -v /var/run/docker.sock:/var/run/docker.sock \
+     gitlab/gitlab-runner:latest
