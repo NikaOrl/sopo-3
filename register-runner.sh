c8d6899001398fcf6ff4de03aa5572f76ef06fd1
Nika <nika.orlova.1997@yandex.ru>
2020-12-06 19:17:23 +0300

    Update register-runner.sh

--- register-runner.sh
+++ register-runner.sh
@@ -1,7 +1,7 @@
 sudo docker exec -it gitlab-runner gitlab-runner register \
   --non-interactive \
   --url http://gitlab \
-  --registration-token spW7RjAniysQ59xsyF79 \
+  --registration-token w7WyKNqwKM7qvxwGCgN \
   --executor docker \
   --docker-image ruby:2.6 \
   --docker-network-mode myNetwork
